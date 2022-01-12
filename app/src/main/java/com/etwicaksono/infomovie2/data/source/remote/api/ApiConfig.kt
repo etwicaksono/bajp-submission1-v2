cd299937a7351e86d1954ec17d33a93cad59209e
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-13 00:25:54 +0700

    update ApiConfig

--- app/src/main/java/com/etwicaksono/infomovie2/data/source/remote/api/ApiConfig.kt
+++ app/src/main/java/com/etwicaksono/infomovie2/data/source/remote/api/ApiConfig.kt
@@ -1,4 +1,42 @@
 package com.etwicaksono.infomovie2.data.source.remote.api
 
+import com.etwicaksono.infomovie2.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class ApiConfig {
+    companion object {
+        fun getInstance(): ApiService {
+            val access_token = BuildConfig.TMDB_KEY
+
+            val loggingInterceptor = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            } else {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor { chain ->
+                    val request = chain.request()
+                    val builder = request.newBuilder()
+                        .header("Authorization", access_token)
+                        .method(request.method, request.body)
+                    val mutatedRequest = builder.build()
+                    val response = chain.proceed(mutatedRequest)
+                    response
+                }
+                .addInterceptor(loggingInterceptor)
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://api.themoviedb.org/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+
+            return retrofit.create(ApiService::class.java)
+        }
+    }
 }
