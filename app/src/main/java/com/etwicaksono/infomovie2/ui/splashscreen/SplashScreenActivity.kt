20fc1a01583b3ed0645794887197921de39db7df
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-13 01:20:36 +0700

    add feature get request token

--- app/src/main/java/com/etwicaksono/infomovie2/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/etwicaksono/infomovie2/ui/splashscreen/SplashScreenActivity.kt
@@ -1,21 +1,34 @@
 package com.etwicaksono.infomovie2.ui.splashscreen
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.etwicaksono.infomovie2.R
 import com.etwicaksono.infomovie2.ui.HomeActivity
 
 class SplashScreenActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this@SplashScreenActivity, HomeActivity::class.java))
-            finish()
-        }, 2000)
+        val viewModel: SplashScreenViewModel by viewModels()
+
+        viewModel.isLoading.observe(this, { loading: Boolean ->
+            if (!loading) {
+                val sharedPref = this.getSharedPreferences(
+                    getString(R.string.preference_file_key),
+                    Context.MODE_PRIVATE
+                )
+                val editor = sharedPref.edit()
+                editor.putString(getString(R.string.saved_request_token),viewModel.requestToken?.requestToken)
+                editor.apply()
+
+                startActivity(Intent(this@SplashScreenActivity, HomeActivity::class.java))
+                finish()
+            }
+        })
     }
 }
--- app/src/main/java/com/etwicaksono/infomovie2/ui/splashscreen/SplashScreenViewModel.kt
+++ app/src/main/java/com/etwicaksono/infomovie2/ui/splashscreen/SplashScreenViewModel.kt
@@ -3,45 +3,49 @@ package com.etwicaksono.infomovie2.ui.splashscreen
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.etwicaksono.infomovie2.data.source.remote.api.ApiConfig
 import com.etwicaksono.infomovie2.data.source.remote.response.ResponseRequestToken
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class SplashScreenViewModel {
+class SplashScreenViewModel : ViewModel() {
     private val _isLoading = MutableLiveData<Boolean>()
-    val isLoading:LiveData<Boolean> = _isLoading
+    val isLoading: LiveData<Boolean> = _isLoading
+    var requestToken: ResponseRequestToken? = null
 
     init {
         getRequestToken()
     }
 
     private fun getRequestToken() {
-        _isLoading.value=true
+        _isLoading.value = true
         val client = api.getRequestToken()
-        client.enqueue(object: Callback<ResponseRequestToken>{
+        client.enqueue(object : Callback<ResponseRequestToken> {
             override fun onResponse(
                 call: Call<ResponseRequestToken>,
                 response: Response<ResponseRequestToken>
             ) {
-                _isLoading.value=false
-                if(!response.isSuccessful){
-                    Log.e(TAG,"getRequestToken onResponse failure: ${response.message()}")
-                    Log.e(TAG,"getRequestToken onResponse failure: ${response.errorBody()}")
+                _isLoading.value = false
+                if (!response.isSuccessful) {
+                    Log.e(TAG, "getRequestToken onResponse failure: ${response.message()}")
+                    Log.e(TAG, "getRequestToken onResponse failure: ${response.errorBody()}")
+                } else {
+                    requestToken = response.body()
                 }
             }
 
             override fun onFailure(call: Call<ResponseRequestToken>, t: Throwable) {
-                _isLoading.value=false
-                Log.e(TAG,"getRequestToken onFailure: ${t.message.toString()}")
+                _isLoading.value = false
+                Log.e(TAG, "getRequestToken onFailure: ${t.message.toString()}")
             }
 
         })
     }
 
-    companion object{
+    companion object {
         private val TAG = SplashScreenViewModel::class.java.simpleName
-        private val api=ApiConfig.getApiService()
+        private val api = ApiConfig.getApiService()
     }
 }
