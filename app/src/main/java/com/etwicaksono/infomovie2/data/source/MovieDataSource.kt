5243a16231778751c2a88ef104538c4c36e26665
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-13 07:03:29 +0700

    update MovieDataSource

--- app/src/main/java/com/etwicaksono/infomovie2/data/source/MovieDataSource.kt
+++ app/src/main/java/com/etwicaksono/infomovie2/data/source/MovieDataSource.kt
@@ -1,3 +1,11 @@
 package com.etwicaksono.infomovie2.data.source
 
-interface MovieDataSource
+import androidx.lifecycle.LiveData
+import com.etwicaksono.infomovie2.data.MovieEntity
+
+interface MovieDataSource {
+    fun getPopularMovies(): LiveData<List<MovieEntity>>
+    fun getMovieDetail(): LiveData<MovieEntity>
+    fun getPopularTvShow(): LiveData<List<MovieEntity>>
+    fun getTvShowDetail(): LiveData<MovieEntity>
+}
