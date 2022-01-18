4bf5782ce687a28382d8f7e97dd19d8f454d2964
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-19 00:43:37 +0700

    add response detail tv

---
+++ app/src/main/java/com/etwicaksono/infomovie2/data/source/remote/response/ResponseDetailTv.kt
@@ -0,0 +1,25 @@
+package com.etwicaksono.infomovie2.data.source.remote.response
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseDetailTv(
+    @SerializedName("backdrop_path")
+    val backdropPath: String?,
+    @SerializedName("episode_run_time")
+    val runtime: List<Int>?,
+    @SerializedName("first_air_date")
+    val releaseDate: String?,
+    @SerializedName("genres")
+    val genres: List<Genre>?,
+    @SerializedName("id")
+    val id: Int?,
+    @SerializedName("name")
+    val title: String?,
+    @SerializedName("overview")
+    val overview: String?,
+    @SerializedName("poster_path")
+    val posterPath: String?,
+    @SerializedName("vote_average")
+    val voteAverage: Double?,
+)
