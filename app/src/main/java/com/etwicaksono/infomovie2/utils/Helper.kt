25fbd288271fe12f9a98bc5a19d8c0205b6c6236
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-18 19:35:43 +0700

    add setImageWithGlide

--- app/src/main/java/com/etwicaksono/infomovie2/utils/Helper.kt
+++ app/src/main/java/com/etwicaksono/infomovie2/utils/Helper.kt
@@ -1,11 +1,25 @@
 package com.etwicaksono.infomovie2.utils
 
 import android.content.Context
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.etwicaksono.infomovie2.R
 import java.io.IOException
 import kotlin.math.floor
 
 object Helper {
+
+    const val TYPE_MOVIE = "TYPE_MOVIE"
+    const val TYPE_TVSHOW = "TYPE_TVSHOW"
+    const val API_IMAGE_ENDPOINT = "https://image.tmdb.org/t/p/"
+    const val ENDPOINT_POSTER_SIZE_W185 = "w185"
+    const val ENDPOINT_POSTER_SIZE_W780 = "w780"
+
+    fun setImageWithGlide(context: Context,imagePath:String,imageView:ImageView){
+        Glide.with(context).clear(imageView)
+        Glide.with(context).load(imagePath).into(imageView)
+    }
+
     fun getJsonDataFromAsset(context: Context, fileName: String): String? {
         val jsonString: String
         try {
