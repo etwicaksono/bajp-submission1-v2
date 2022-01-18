547fd8cb581123aa77486dea1d0a5b8340da80e1
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-19 00:32:47 +0700

    update collapse toolbar

--- app/src/main/java/com/etwicaksono/infomovie2/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/etwicaksono/infomovie2/ui/detail/DetailActivity.kt
@@ -1,16 +1,21 @@
 package com.etwicaksono.infomovie2.ui.detail
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ShareCompat
+import androidx.core.view.ViewCompat
 import androidx.lifecycle.ViewModelProvider
+import com.etwicaksono.infomovie2.R
 import com.etwicaksono.infomovie2.databinding.ActivityDetailBinding
 import com.etwicaksono.infomovie2.utils.Helper
 import com.etwicaksono.infomovie2.utils.Helper.API_IMAGE_ENDPOINT
 import com.etwicaksono.infomovie2.utils.Helper.ENDPOINT_POSTER_SIZE_W780
-import com.etwicaksono.infomovie2.utils.Helper.getRuntime
+import com.etwicaksono.infomovie2.utils.Helper.TYPE_MOVIE
 import com.etwicaksono.infomovie2.utils.Helper.setImageWithGlide
 import com.etwicaksono.infomovie2.viewmodel.ViewModelFactory
+import com.google.android.material.appbar.AppBarLayout
 
 class DetailActivity : AppCompatActivity() {
 
@@ -21,8 +26,7 @@ class DetailActivity : AppCompatActivity() {
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setUpToolbar()
 
 
         val id = intent.getIntExtra(EXTRA_ID, 1)
@@ -111,6 +115,38 @@ class DetailActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    private fun setUpToolbar() {
+        binding.apply {
+            appBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+                if (collapseToolbar.height + verticalOffset < 2 * ViewCompat.getMinimumHeight(
+                        collapseToolbar
+                    )
+                ) {
+                    collapseToolbar.setCollapsedTitleTextColor(Color.BLACK)
+                    toolbar.apply {
+                        setBackgroundColor(Color.WHITE)
+                        title =
+                            if (intent.getStringExtra(EXTRA_TYPE) == TYPE_MOVIE) context.getString(R.string.detail_movie) else context.getString(
+                                R.string.detail_tv_show
+                            )
+                        visibility = View.VISIBLE
+                    }
+                } else {
+                    collapseToolbar.setExpandedTitleColor(Color.TRANSPARENT)
+                    toolbar.apply {
+                        setBackgroundColor(Color.TRANSPARENT)
+                        visibility = View.GONE
+                    }
+                }
+            })
+
+            toolbar.apply {
+                setNavigationIcon(R.drawable.ic_back)
+                setNavigationOnClickListener { onBackPressed() }
+            }
+        }
     }
 
     companion object {
