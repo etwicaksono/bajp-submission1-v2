8954114bb2bd6c0d01befac9a0b0e0739c1658d9
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-20 05:38:09 +0700

    remove iddling resource

--- app/src/main/java/com/etwicaksono/infomovie2/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/etwicaksono/infomovie2/data/source/remote/RemoteDataSource.kt
@@ -5,17 +5,13 @@ import com.etwicaksono.infomovie2.data.source.remote.response.ResponseDetailMovi
 import com.etwicaksono.infomovie2.data.source.remote.response.ResponseDetailTv
 import com.etwicaksono.infomovie2.data.source.remote.response.ResponseMovieItem
 import com.etwicaksono.infomovie2.data.source.remote.response.ResponseTvShowItem
-import com.etwicaksono.infomovie2.utils.EspressoIdlingResource.decrement
-import com.etwicaksono.infomovie2.utils.EspressoIdlingResource.increment
 import retrofit2.await
 
 class RemoteDataSource {
 
     suspend fun getPopularMovies(callback: LoadMoviesCallback) {
-        increment()
         api.getPopularMovie().await().result.let {
             callback.onPopularMoviesReceived(it)
-            decrement()
         }
     }
 
@@ -24,10 +20,8 @@ class RemoteDataSource {
     }
 
     suspend fun getPopularTvShow(callback: LoadTvShowsCallback) {
-        increment()
         api.getPopularTvShow().await().result.let {
             callback.onPopularTvShowsReceived(it)
-            decrement()
         }
     }
 
@@ -36,10 +30,8 @@ class RemoteDataSource {
     }
 
     suspend fun getMovieDetail(movieId: Int, callback: LoadMovieDetailCallback) {
-        increment()
         api.getDetailMovie(movieId).await().let {
             callback.onMoviedetailReceived(it)
-            decrement()
         }
     }
 
@@ -48,10 +40,8 @@ class RemoteDataSource {
     }
 
     suspend fun getTvDetail(tvId: Int, callback: LoadTvDetailCallback) {
-        increment()
         api.getDetailTv(tvId).await().let {
             callback.onTvDetailReceived(it)
-            decrement()
         }
     }
 
