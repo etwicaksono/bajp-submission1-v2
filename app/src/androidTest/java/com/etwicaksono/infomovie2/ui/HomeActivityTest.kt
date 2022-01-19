96f26d044c1fdfd58db0b14a575af2110de34d30
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-20 04:35:36 +0700

    setup HomeActivityTest

--- app/src/androidTest/java/com/etwicaksono/infomovie2/ui/HomeActivityTest.kt
+++ app/src/androidTest/java/com/etwicaksono/infomovie2/ui/HomeActivityTest.kt
@@ -1,41 +1,51 @@
 package com.etwicaksono.infomovie2.ui
 
-import android.content.Context
 import androidx.recyclerview.widget.RecyclerView
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import com.etwicaksono.infomovie2.R
 import com.etwicaksono.infomovie2.utils.DataDummy
+import com.etwicaksono.infomovie2.utils.EspressoIdlingResource
+import com.etwicaksono.infomovie2.utils.Helper.getRuntime
 import org.hamcrest.CoreMatchers.allOf
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 import java.lang.Thread.sleep
 
-/*
-@RunWith(AndroidJUnit4::class)
 class HomeActivityTest {
-    private val context: Context = ApplicationProvider.getApplicationContext()
-    private val dummyMovie = DataDummy.getAllMovies()
-    private val dummyTvShow = DataDummy.getAllTvShows()
+    private val dummyListMovie = DataDummy.getAllMoviesDummy()
+    private val dummyListTvShow = DataDummy.getAllTvShowsDummy()
+    private val dummyMovie = DataDummy.getMovieDummy()
+    private val dummyTvShow = DataDummy.getTvShowDummy()
 
-    @Rule
-    @JvmField
-    var activityRule: ActivityTestRule<HomeActivity> = ActivityTestRule(HomeActivity::class.java)
+    @get:Rule
+    var activityRule = ActivityTestRule(HomeActivity::class.java)
+
+    @Before
+    fun setup() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.espressoTestIdlingResource)
+    }
+
+    @After
+    fun tearDown() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.espressoTestIdlingResource)
+    }
 
 
     @Test
     fun loadMovie() {
+        onView(withId(R.id.rv_film)).check(matches(isDisplayed()))
         onView(allOf(withId(R.id.rv_film), isDisplayed())).perform(
             RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
-                dummyMovie.size
+                dummyListMovie.size
             )
         )
     }
@@ -48,34 +58,22 @@ class HomeActivityTest {
                 0, click()
             )
         )
-        onView(withId(R.id.tv_header)).apply {
-            check(matches(isDisplayed()))
-            check(matches(withText(context.resources.getString(R.string.detail_movie))))
-        }
         onView(withId(R.id.tv_title)).apply {
             check(matches(isDisplayed()))
-            check(matches(withText(dummyMovie[0].title)))
+            check(matches(withText(dummyListMovie[0].title)))
         }
         onView(withId(R.id.tv_genre)).apply {
             check(matches(isDisplayed()))
-            check(matches(withText(dummyMovie[0].genres)))
+            check(matches(withText(dummyMovie.genres)))
         }
         onView(withId(R.id.tv_release_date)).apply {
             check(matches(isDisplayed()))
-            check(matches(withText(dummyMovie[0].releaseDate)))
+            check(matches(withText(dummyMovie.releaseDate)))
         }
         onView(withId(R.id.iv_poster)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_director)).apply {
-            check(matches(isDisplayed()))
-            check(matches(withText(dummyMovie[0].director)))
-        }
-        onView(withId(R.id.tv_actors)).apply {
-            check(matches(isDisplayed()))
-            check(matches(withText(dummyMovie[0].actors)))
-        }
         onView(withId(R.id.tv_vote)).apply {
             check(matches(isDisplayed()))
-            check(matches(withText(dummyMovie[0].runtime?.let {
+            check(matches(withText(dummyMovie.runtime?.let {
                 getRuntime(
                     it.toInt()
                 )
@@ -83,7 +81,7 @@ class HomeActivityTest {
         }
         onView(withId(R.id.tv_plot)).apply {
             check(matches(isDisplayed()))
-            check(matches(withText(dummyMovie[0].plot)))
+            check(matches(withText(dummyMovie.overview)))
         }
         onView(withId(R.id.fab)).check(matches(isDisplayed()))
     }
@@ -95,7 +93,7 @@ class HomeActivityTest {
         sleep(1000)
         onView(allOf(withId(R.id.rv_film), isDisplayed())).perform(
             RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
-                dummyTvShow.size
+                dummyListTvShow.size
             )
         )
     }
@@ -110,34 +108,22 @@ class HomeActivityTest {
                 0, click()
             )
         )
-        onView(withId(R.id.tv_header)).apply {
-            check(matches(isDisplayed()))
-            check(matches(withText(context.resources.getString(R.string.detail_tv_show))))
-        }
         onView(withId(R.id.tv_title)).apply {
             check(matches(isDisplayed()))
-            check(matches(withText(dummyTvShow[0].title)))
+            check(matches(withText(dummyTvShow.title)))
         }
         onView(withId(R.id.tv_genre)).apply {
             check(matches(isDisplayed()))
-            check(matches(withText(dummyTvShow[0].genres)))
+            check(matches(withText(dummyTvShow.genres)))
         }
         onView(withId(R.id.tv_release_date)).apply {
             check(matches(isDisplayed()))
-            check(matches(withText(dummyTvShow[0].releaseDate)))
+            check(matches(withText(dummyTvShow.releaseDate)))
         }
         onView(withId(R.id.iv_poster)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_director)).apply {
-            check(matches(isDisplayed()))
-            check(matches(withText(dummyTvShow[0].director)))
-        }
-        onView(withId(R.id.tv_actors)).apply {
-            check(matches(isDisplayed()))
-            check(matches(withText(dummyTvShow[0].actors)))
-        }
         onView(withId(R.id.tv_vote)).apply {
             check(matches(isDisplayed()))
-            check(matches(withText(dummyTvShow[0].runtime?.let {
+            check(matches(withText(dummyTvShow.runtime?.let {
                 getRuntime(
                     it.toInt()
                 )
@@ -145,8 +131,8 @@ class HomeActivityTest {
         }
         onView(withId(R.id.tv_plot)).apply {
             check(matches(isDisplayed()))
-            check(matches(withText(dummyTvShow[0].plot)))
+            check(matches(withText(dummyTvShow.overview)))
         }
         onView(withId(R.id.fab)).check(matches(isDisplayed()))
     }
-}*/
+}
