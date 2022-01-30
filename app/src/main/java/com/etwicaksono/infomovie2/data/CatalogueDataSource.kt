fc774f28a85b10d289060a55182a053c8f2c8dd8
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-30 14:51:00 +0700

    update CatalogueDataSource.kt

--- app/src/main/java/com/etwicaksono/infomovie2/data/CatalogueDataSource.kt
+++ app/src/main/java/com/etwicaksono/infomovie2/data/CatalogueDataSource.kt
@@ -1,12 +1,16 @@
 package com.etwicaksono.infomovie2.data
 
 import androidx.lifecycle.LiveData
+import androidx.paging.PagedList
 import com.etwicaksono.infomovie2.data.source.local.entity.ListEntity
 import com.etwicaksono.infomovie2.data.source.local.entity.DetailEntity
+import com.etwicaksono.infomovie2.valueobject.Resource
 
 interface CatalogueDataSource {
-    fun getPopularMovies(): LiveData<List<ListEntity>>
-    fun getMovieDetail(movieId:Int): LiveData<DetailEntity>
-    fun getPopularTvShow(): LiveData<List<ListEntity>>
-    fun getTvShowDetail(tvId:Int): LiveData<DetailEntity>
+    fun getPopularMovies(): LiveData<Resource<PagedList<ListEntity>>>
+    fun getMovieDetail(movieId:Int): LiveData<Resource<DetailEntity>>
+    fun getPopularTvShow(): LiveData<Resource<PagedList<ListEntity>>>
+    fun getTvShowDetail(tvId:Int): LiveData<Resource<DetailEntity>>
+    fun getFavorite():LiveData<PagedList<ListEntity>>
+    fun setFavorite(catalogue:ListEntity,state:Boolean)
 }
