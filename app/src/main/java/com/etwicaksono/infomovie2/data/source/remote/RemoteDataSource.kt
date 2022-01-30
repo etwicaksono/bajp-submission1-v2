cd904a71201cf710cafe52a524ca9fd2c2368c10
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-30 12:53:13 +0700

    update RemoteDataSource.kt

--- app/src/main/java/com/etwicaksono/infomovie2/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/etwicaksono/infomovie2/data/source/remote/RemoteDataSource.kt
@@ -1,52 +1,112 @@
 package com.etwicaksono.infomovie2.data.source.remote
 
+import android.content.ContentValues
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.etwicaksono.infomovie2.data.source.remote.api.ApiConfig
-import com.etwicaksono.infomovie2.data.source.remote.response.ResponseDetailMovie
-import com.etwicaksono.infomovie2.data.source.remote.response.ResponseDetailTv
-import com.etwicaksono.infomovie2.data.source.remote.response.ResponseMovieItem
-import com.etwicaksono.infomovie2.data.source.remote.response.ResponseTvShowItem
-import retrofit2.await
+import com.etwicaksono.infomovie2.data.source.remote.response.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class RemoteDataSource {
 
-    suspend fun getPopularMovies(callback: LoadMoviesCallback) {
-        api.getPopularMovie().await().result.let {
-            callback.onPopularMoviesReceived(it)
-        }
+    fun getPopularMovies(): LiveData<ApiResponse<List<ResponseMovieItem>>> {
+        val resultMovies = MutableLiveData<ApiResponse<List<ResponseMovieItem>>>()
+        api.getPopularMovie().enqueue(object : Callback<ResponseCatalogue<ResponseMovieItem>> {
+            override fun onResponse(
+                call: Call<ResponseCatalogue<ResponseMovieItem>>,
+                response: Response<ResponseCatalogue<ResponseMovieItem>>
+            ) {
+                if (response.isSuccessful) {
+                    resultMovies.value = response.body()?.let { ApiResponse.success(it.result) }
+                } else {
+                    Log.e(ContentValues.TAG, "onFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseCatalogue<ResponseMovieItem>>, t: Throwable) {
+                Log.e(ContentValues.TAG, "onFailure: ${t.message.toString()}")
+            }
+
+        })
+
+        return resultMovies
     }
 
-    interface LoadMoviesCallback {
-        fun onPopularMoviesReceived(res: List<ResponseMovieItem>)
+    suspend fun getPopularTvShow(): LiveData<ApiResponse<List<ResponseTvShowItem>>> {
+        val resultTvShow = MutableLiveData<ApiResponse<List<ResponseTvShowItem>>>()
+        api.getPopularTvShow().enqueue(object : Callback<ResponseCatalogue<ResponseTvShowItem>> {
+            override fun onResponse(
+                call: Call<ResponseCatalogue<ResponseTvShowItem>>,
+                response: Response<ResponseCatalogue<ResponseTvShowItem>>
+            ) {
+                if (response.isSuccessful) {
+                    resultTvShow.value = response.body()?.let { ApiResponse.success(it.result) }
+                } else {
+                    Log.e(ContentValues.TAG, "onFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(
+                call: Call<ResponseCatalogue<ResponseTvShowItem>>,
+                t: Throwable
+            ) {
+                Log.e(ContentValues.TAG, "onFailure: ${t.message.toString()}")
+            }
+
+        })
+
+        return resultTvShow
     }
 
-    suspend fun getPopularTvShow(callback: LoadTvShowsCallback) {
-        api.getPopularTvShow().await().result.let {
-            callback.onPopularTvShowsReceived(it)
-        }
+
+    fun getMovieDetail(movieId: Int): LiveData<ApiResponse<ResponseDetailMovie>> {
+        val resultDetail = MutableLiveData<ApiResponse<ResponseDetailMovie>>()
+        api.getDetailMovie(movieId).enqueue(object : Callback<ResponseDetailMovie> {
+            override fun onResponse(
+                call: Call<ResponseDetailMovie>,
+                response: Response<ResponseDetailMovie>
+            ) {
+                if (response.isSuccessful) {
+                    resultDetail.value = response.body()?.let { ApiResponse.success(it) }
+                } else {
+                    Log.e(ContentValues.TAG, "onFailure:${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseDetailMovie>, t: Throwable) {
+                Log.e(ContentValues.TAG, "onFailure: ${t.message.toString()}")
+            }
+
+        })
+
+        return resultDetail
     }
 
-    interface LoadTvShowsCallback {
-        fun onPopularTvShowsReceived(res: List<ResponseTvShowItem>)
-    }
 
-    suspend fun getMovieDetail(movieId: Int, callback: LoadMovieDetailCallback) {
-        api.getDetailMovie(movieId).await().let {
-            callback.onMoviedetailReceived(it)
-        }
-    }
+    fun getTvDetail(tvId: Int):LiveData<ApiResponse<ResponseDetailTv>>{
+        val resultDetail = MutableLiveData<ApiResponse<ResponseDetailTv>>()
+        api.getDetailTv(tvId).enqueue(object : Callback<ResponseDetailTv> {
+            override fun onResponse(
+                call: Call<ResponseDetailTv>,
+                response: Response<ResponseDetailTv>
+            ) {
+                if (response.isSuccessful) {
+                    resultDetail.value = response.body()?.let { ApiResponse.success(it) }
+                } else {
+                    Log.e(ContentValues.TAG, "onFailure:${response.message()}")
+                }
+            }
 
-    interface LoadMovieDetailCallback {
-        fun onMoviedetailReceived(res: ResponseDetailMovie)
-    }
+            override fun onFailure(call: Call<ResponseDetailTv>, t: Throwable) {
+                Log.e(ContentValues.TAG, "onFailure: ${t.message.toString()}")
+            }
 
-    suspend fun getTvDetail(tvId: Int, callback: LoadTvDetailCallback) {
-        api.getDetailTv(tvId).await().let {
-            callback.onTvDetailReceived(it)
-        }
-    }
+        })
 
-    interface LoadTvDetailCallback {
-        fun onTvDetailReceived(res: ResponseDetailTv)
+        return resultDetail
     }
 
 
