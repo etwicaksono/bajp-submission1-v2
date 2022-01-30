bb841c07ab2f8718641670a2078374b7e8544f3f
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-30 14:46:38 +0700

    setup NetworkBoundResource.kt

--- app/src/main/java/com/etwicaksono/infomovie2/data/NetworkBoundResource.kt
+++ app/src/main/java/com/etwicaksono/infomovie2/data/NetworkBoundResource.kt
@@ -1,4 +1,60 @@
 package com.etwicaksono.infomovie2.data
 
-class NetworkBoundResource {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import com.etwicaksono.infomovie2.data.source.remote.ApiResponse
+import com.etwicaksono.infomovie2.data.source.remote.StatusResponse
+import com.etwicaksono.infomovie2.utils.AppExecutors
+import com.etwicaksono.infomovie2.valueobject.Resource
+
+abstract class NetworkBoundResource<ResultType, RequestType>(private val mExecutors: AppExecutors) {
+    private val result = MediatorLiveData<Resource<ResultType>>()
+
+    init {
+        result.value = Resource.loading(null)
+    }
+
+    protected open fun onFetchFailed() {}
+
+    protected abstract fun loadFromDB(): LiveData<ResultType>
+
+    protected abstract fun shouldFetch(data: ResultType?): Boolean
+
+    protected abstract fun createCall(): LiveData<ApiResponse<RequestType>>
+
+    protected abstract fun saveCallResult(data: RequestType)
+
+    private fun fetchFromNetwork(dbSource: LiveData<ResultType>) {
+        val apiResponse = createCall()
+
+        result.addSource(dbSource) { newData -> result.value = Resource.loading(newData) }
+
+        result.addSource(apiResponse) { response ->
+            result.removeSource(apiResponse)
+            result.removeSource(dbSource)
+            when (response.status) {
+                StatusResponse.SUCCESS -> mExecutors.diskIO().execute {
+                    saveCallResult(response.body)
+                    mExecutors.mainThread().execute {
+                        result.addSource(loadFromDB()) { newData ->
+                            result.value = Resource.success(newData)
+                        }
+                    }
+                }
+                StatusResponse.EMPTY -> mExecutors.mainThread().execute {
+                    result.addSource(loadFromDB()) { newData ->
+                        result.value = Resource.success(newData)
+                    }
+                }
+                StatusResponse.ERROR -> {
+                    onFetchFailed()
+                    result.addSource(dbSource) { newData ->
+                        result.value = Resource.error(response.message, newData)
+                    }
+                }
+            }
+        }
+    }
+
+    fun asLiveData(): LiveData<Resource<ResultType>> = result
 }
