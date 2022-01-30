ec35431b801ec69202a81a96ed11feed8bc1f5c8
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-30 16:00:42 +0700

    update DetailActivity.kt

--- app/src/main/java/com/etwicaksono/infomovie2/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/etwicaksono/infomovie2/ui/detail/DetailActivity.kt
@@ -3,24 +3,28 @@ package com.etwicaksono.infomovie2.ui.detail
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ShareCompat
 import androidx.core.view.ViewCompat
 import androidx.lifecycle.ViewModelProvider
 import com.etwicaksono.infomovie2.R
+import com.etwicaksono.infomovie2.data.source.local.entity.DetailEntity
 import com.etwicaksono.infomovie2.databinding.ActivityDetailBinding
-import com.etwicaksono.infomovie2.utils.Helper
 import com.etwicaksono.infomovie2.utils.Helper.API_IMAGE_ENDPOINT
 import com.etwicaksono.infomovie2.utils.Helper.ENDPOINT_POSTER_SIZE_W185
 import com.etwicaksono.infomovie2.utils.Helper.ENDPOINT_POSTER_SIZE_W780
 import com.etwicaksono.infomovie2.utils.Helper.TYPE_MOVIE
+import com.etwicaksono.infomovie2.utils.Helper.TYPE_TV
 import com.etwicaksono.infomovie2.utils.Helper.setImageWithGlide
+import com.etwicaksono.infomovie2.valueobject.Status
 import com.etwicaksono.infomovie2.viewmodel.ViewModelFactory
 import com.google.android.material.appbar.AppBarLayout
 
 class DetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailBinding
+    private lateinit var viewModel: DetailViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,92 +38,48 @@ class DetailActivity : AppCompatActivity() {
         val type = intent.getStringExtra(EXTRA_TYPE)
 
         val factory = ViewModelFactory.getInstance()
-        val viewModel = ViewModelProvider(
+        viewModel = ViewModelProvider(
             this,
             factory
         )[DetailViewModel::class.java]
 
         when (type) {
             TYPE_MOVIE -> {
-                viewModel.getMovieDetail(id).observe(this, { movie ->
-                    binding.apply {
-                        tvTitle.text = movie.title
-                        setImageWithGlide(
-                            this@DetailActivity,
-                            API_IMAGE_ENDPOINT + ENDPOINT_POSTER_SIZE_W185 + movie.posterPath,
-                            ivPoster
-                        )
-                        setImageWithGlide(
-                            this@DetailActivity,
-                            API_IMAGE_ENDPOINT + ENDPOINT_POSTER_SIZE_W780 + movie.backdropPath,
-                            ivBackDrop
-                        )
-                        tvReleaseDate.text = movie.releaseDate
-                        tvVote.text = movie.voteAverage
-                        tvGenre.text = movie.genres
-                        tvPlot.text = movie.overview
-                        tvRuntime.text = movie.runtime
-
-                        fab.setOnClickListener {
-                            val selected = if (type == "movies") "movie" else "tv show"
-                            val mimeType = "text/plain"
-                            ShareCompat.IntentBuilder
-                                .from(this@DetailActivity)
-                                .setType(mimeType)
-                                .setChooserTitle("Bagikan $selected ini sekarang!")
-                                .setText(
-                                    """
-                        Ada $selected seru loh, yuk nonton sekarang! Nih aku kasih infonya.
-                        Movie : ${movie.title}
-                        Tahun : ${movie.year}
-                        Sinopsis : ${movie.overview}
-                    """.trimIndent()
-                                )
-                                .startChooser()
+                viewModel.getMovieDetail(id).observe(this) { movie ->
+                    if (movie != null) {
+                        when (movie.status) {
+                            Status.LOADING -> showLoading(true)
+                            Status.SUCCESS -> {
+                                showLoading(false)
+                                movie.data?.let { setView(it) }
+                            }
+                            Status.ERROR -> {
+                                showLoading(false)
+                                Toast.makeText(this, "terjadi kesalahan!", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
                         }
                     }
-                })
+                }
 
             }
-            Helper.TYPE_TV -> {
-                viewModel.getTvDetail(id).observe(this, { movie ->
-                    binding.apply {
-                        tvTitle.text = movie.title
-                        setImageWithGlide(
-                            this@DetailActivity,
-                            API_IMAGE_ENDPOINT + ENDPOINT_POSTER_SIZE_W185 + movie.posterPath,
-                            ivPoster
-                        )
-                        setImageWithGlide(
-                            this@DetailActivity,
-                            API_IMAGE_ENDPOINT + ENDPOINT_POSTER_SIZE_W780 + movie.backdropPath,
-                            ivBackDrop
-                        )
-                        tvReleaseDate.text = movie.releaseDate
-                        tvVote.text = movie.voteAverage
-                        tvGenre.text = movie.genres
-                        tvPlot.text = movie.overview
-                        tvRuntime.text = movie.runtime
-
-                        fab.setOnClickListener {
-                            val selected = if (type == "movies") "movie" else "tv show"
-                            val mimeType = "text/plain"
-                            ShareCompat.IntentBuilder
-                                .from(this@DetailActivity)
-                                .setType(mimeType)
-                                .setChooserTitle("Bagikan $selected ini sekarang!")
-                                .setText(
-                                    """
-                        Ada $selected seru loh, yuk nonton sekarang! Nih aku kasih infonya.
-                        Movie : ${movie.title}
-                        Tahun : ${movie.year}
-                        Sinopsis : ${movie.overview}
-                    """.trimIndent()
-                                )
-                                .startChooser()
+            TYPE_TV -> {
+                viewModel.getTvDetail(id).observe(this) { movie ->
+                    if (movie != null) {
+                        when (movie.status) {
+                            Status.LOADING -> showLoading(true)
+                            Status.SUCCESS -> {
+                                showLoading(false)
+                                movie.data?.let { setView(it) }
+                            }
+                            Status.ERROR -> {
+                                showLoading(false)
+                                Toast.makeText(this, "terjadi kesalahan!", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
                         }
                     }
-                })
+                }
 
             }
         }
@@ -158,6 +118,53 @@ class DetailActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun showLoading(show: Boolean) {
+        if (show) {
+            binding.progressBar?.visibility = View.VISIBLE
+        } else {
+            binding.progressBar?.visibility = View.GONE
+        }
+    }
+
+    private fun setView(data: DetailEntity) {
+        binding.apply {
+            tvTitle.text = data.title
+            setImageWithGlide(
+                this@DetailActivity,
+                API_IMAGE_ENDPOINT + ENDPOINT_POSTER_SIZE_W185 + data.posterPath,
+                ivPoster
+            )
+            setImageWithGlide(
+                this@DetailActivity,
+                API_IMAGE_ENDPOINT + ENDPOINT_POSTER_SIZE_W780 + data.backdropPath,
+                ivBackDrop
+            )
+            tvReleaseDate.text = data.releaseDate
+            tvVote.text = data.voteAverage
+            tvGenre.text = data.genres
+            tvPlot.text = data.overview
+            tvRuntime.text = data.runtime
+
+            fab.setOnClickListener {
+                val mimeType = "text/plain"
+                ShareCompat.IntentBuilder
+                    .from(this@DetailActivity)
+                    .setType(mimeType)
+                    .setChooserTitle("Bagikan film ini sekarang!")
+                    .setText(
+                        """
+                        Ada film seru loh, yuk nonton sekarang! Nih aku kasih infonya.
+                        Movie : ${data.title}
+                        Tahun : ${data.year}
+                        Sinopsis : ${data.overview}
+                    """.trimIndent()
+                    )
+                    .startChooser()
+            }
+        }
+    }
+
 
     companion object {
         const val EXTRA_ID = "extra_id"
