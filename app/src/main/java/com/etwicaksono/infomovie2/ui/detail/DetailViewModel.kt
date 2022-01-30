fbe0c3cde25be137010ef5997da0b3c4eaab17c4
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-30 15:32:35 +0700

    update DetailViewModel.kt

--- app/src/main/java/com/etwicaksono/infomovie2/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/etwicaksono/infomovie2/ui/detail/DetailViewModel.kt
@@ -2,14 +2,19 @@ package com.etwicaksono.infomovie2.ui.detail
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import com.etwicaksono.infomovie2.data.source.local.entity.DetailEntity
 import com.etwicaksono.infomovie2.data.CatalogueRepository
+import com.etwicaksono.infomovie2.data.source.local.entity.DetailEntity
+import com.etwicaksono.infomovie2.data.source.local.entity.ListEntity
+import com.etwicaksono.infomovie2.valueobject.Resource
 
 class DetailViewModel(private val catalogueRepository: CatalogueRepository) : ViewModel() {
 
-    fun getMovieDetail(movieId: Int): LiveData<DetailEntity> =
+    fun getMovieDetail(movieId: Int): LiveData<Resource<DetailEntity>> =
         catalogueRepository.getMovieDetail(movieId)
 
-    fun getTvDetail(tvId: Int): LiveData<DetailEntity> =
+    fun getTvDetail(tvId: Int): LiveData<Resource<DetailEntity>> =
         catalogueRepository.getTvShowDetail(tvId)
+
+    fun setFavorite(catalogue: ListEntity, newState: Boolean) =
+        catalogueRepository.setFavorite(catalogue, newState)
 }
