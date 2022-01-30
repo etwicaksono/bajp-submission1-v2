ca3dd3d65a629034ccd7a3bd6e2466021aa76175
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-30 16:06:47 +0700

    update ListAdapter.kt

--- app/src/main/java/com/etwicaksono/infomovie2/ui/list/ListAdapter.kt
+++ app/src/main/java/com/etwicaksono/infomovie2/ui/list/ListAdapter.kt
@@ -2,15 +2,43 @@ package com.etwicaksono.infomovie2.ui.list
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.etwicaksono.infomovie2.data.source.local.entity.ListEntity
 import com.etwicaksono.infomovie2.databinding.ItemsRowMovieBinding
 import com.etwicaksono.infomovie2.utils.Helper
 
 class ListAdapter(private val onClick: (ListEntity) -> Unit) :
-    RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
+    PagedListAdapter<ListEntity, ListAdapter.ViewHolder>(DIFF_CALLBACK) {
 
-    inner class ListViewHolder(private val binding: ItemsRowMovieBinding) :
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListAdapter.ViewHolder {
+        val binding =
+            ItemsRowMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ListAdapter.ViewHolder, position: Int) {
+        val movie = getItem(position)
+        if (movie != null) {
+            holder.bind(movie)
+        }
+    }
+
+
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<ListEntity>() {
+            override fun areItemsTheSame(oldItem: ListEntity, newItem: ListEntity): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: ListEntity, newItem: ListEntity): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+    inner class ViewHolder(private val binding: ItemsRowMovieBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(catalogue: ListEntity) {
             with(binding) {
@@ -31,27 +59,5 @@ class ListAdapter(private val onClick: (ListEntity) -> Unit) :
             }
         }
 
-    }
-
-    private val listMovies = ArrayList<ListEntity>()
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListAdapter.ListViewHolder {
-        val binding =
-            ItemsRowMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ListViewHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: ListAdapter.ListViewHolder, position: Int) {
-        val movie = listMovies[position]
-        holder.bind(movie)
-    }
-
-    override fun getItemCount(): Int = listMovies.size
-
-    fun setList(catalogues: List<ListEntity>?) {
-        if (catalogues == null) return
-        this.listMovies.clear()
-        this.listMovies.addAll(catalogues)
-        notifyDataSetChanged()
     }
 }
