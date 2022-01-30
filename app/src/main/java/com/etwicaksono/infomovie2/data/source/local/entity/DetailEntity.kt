b39cff30ed4c3bd1654715e918371d7e0b0c346a
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-30 11:17:36 +0700

    setup DetailEntity.kt

--- app/src/main/java/com/etwicaksono/infomovie2/data/source/local/entity/DetailEntity.kt
+++ app/src/main/java/com/etwicaksono/infomovie2/data/source/local/entity/DetailEntity.kt
@@ -1,15 +1,32 @@
 package com.etwicaksono.infomovie2.data.source.local.entity
 
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "detail_entities")
 data class DetailEntity(
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "id")
+    val id: Int = 0,
+    @ColumnInfo(name = "backdrop_path")
     val backdropPath: String?,
+    @ColumnInfo(name = "genres")
     val genres: String?,
-    val id: Int?,
+    @ColumnInfo(name = "overview")
     val overview: String?,
+    @ColumnInfo(name = "poster_path")
     val posterPath: String?,
+    @ColumnInfo(name = "release_date")
     val releaseDate: String?,
+    @ColumnInfo(name = "year")
     val year: String?,
+    @ColumnInfo(name = "runtime")
     val runtime: String?,
+    @ColumnInfo(name = "title")
     val title: String?,
+    @ColumnInfo(name = "vote_average")
     val voteAverage: String?,
 )
