2f71702e9531b9179b9acc5d3cf029147f1fa2b2
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-30 15:33:40 +0700

    update ListViewModel.kt

--- app/src/main/java/com/etwicaksono/infomovie2/ui/list/ListViewModel.kt
+++ app/src/main/java/com/etwicaksono/infomovie2/ui/list/ListViewModel.kt
@@ -2,14 +2,16 @@ package com.etwicaksono.infomovie2.ui.list
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.paging.PagedList
 import com.etwicaksono.infomovie2.data.source.local.entity.ListEntity
 import com.etwicaksono.infomovie2.data.CatalogueRepository
+import com.etwicaksono.infomovie2.valueobject.Resource
 
 class ListViewModel(private val repo: CatalogueRepository) : ViewModel() {
 
-    fun getAllMovies(): LiveData<List<ListEntity>> = repo.getPopularMovies()
+    fun getAllMovies(): LiveData<Resource<PagedList<ListEntity>>> = repo.getPopularMovies()
 
-    fun getAllTvShows(): LiveData<List<ListEntity>> = repo.getPopularTvShow()
+    fun getAllTvShows(): LiveData<Resource<PagedList<ListEntity>>> = repo.getPopularTvShow()
 
 
 }
