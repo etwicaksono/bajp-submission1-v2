444bad26bae47120b66137ea81c0dac3f537e41f
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-02-01 02:50:09 +0700

    update CatalogueDao.kt

--- app/src/main/java/com/etwicaksono/infomovie2/data/source/local/room/CatalogueDao.kt
+++ app/src/main/java/com/etwicaksono/infomovie2/data/source/local/room/CatalogueDao.kt
@@ -3,17 +3,18 @@ package com.etwicaksono.infomovie2.data.source.local.room
 import androidx.lifecycle.LiveData
 import androidx.paging.DataSource
 import androidx.room.*
+import androidx.sqlite.db.SupportSQLiteQuery
 import com.etwicaksono.infomovie2.data.source.local.entity.DetailEntity
 import com.etwicaksono.infomovie2.data.source.local.entity.ListEntity
 import com.etwicaksono.infomovie2.utils.Helper
 
 @Dao
 interface CatalogueDao {
-    @Query("SELECT * FROM list_entities WHERE type = '${Helper.TYPE_MOVIE}'")
-    fun getMovies(): DataSource.Factory<Int, ListEntity>
+    @RawQuery(observedEntities = [ListEntity::class])
+    fun getMovies(query:SupportSQLiteQuery): DataSource.Factory<Int, ListEntity>
 
-    @Query("SELECT * FROM list_entities WHERE type = '${Helper.TYPE_TV}'")
-    fun getTvShow(): DataSource.Factory<Int, ListEntity>
+    @RawQuery(observedEntities = [ListEntity::class])
+    fun getTvShow(query:SupportSQLiteQuery): DataSource.Factory<Int, ListEntity>
 
     @Query("SELECT * FROM list_entities JOIN detail_entities ON list_entities.id = detail_entities.id WHERE type = '${Helper.TYPE_MOVIE}' AND detail_entities.favorite = 1")
     fun getFavoriteMovies(): DataSource.Factory<Int, ListEntity>
