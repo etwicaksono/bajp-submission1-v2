3ff40eaa52bd935b425b8e6d9f645bedcded5253
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-02-01 01:21:38 +0700

    add menu

--- app/src/main/java/com/etwicaksono/infomovie2/ui/homelist/HomeActivity.kt
+++ app/src/main/java/com/etwicaksono/infomovie2/ui/homelist/HomeActivity.kt
@@ -2,7 +2,9 @@ package com.etwicaksono.infomovie2.ui.homelist
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import com.etwicaksono.infomovie2.R
 import com.etwicaksono.infomovie2.databinding.ActivityHomeListBinding
 import com.etwicaksono.infomovie2.ui.favoritelist.FavoriteActivity
 import com.etwicaksono.infomovie2.utils.Helper.TAB_TITLES
@@ -15,14 +17,21 @@ class HomeActivity : AppCompatActivity() {
         val binding = ActivityHomeListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
+
         binding.viewPager.adapter = ListPagerAdapter(this)
         TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
             tab.text = TAB_TITLES[position]
         }.attach()
 
         binding.btnFavorite.setOnClickListener {
-            startActivity(Intent(this@HomeActivity,FavoriteActivity::class.java))
+            startActivity(Intent(this@HomeActivity, FavoriteActivity::class.java))
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_home, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
 }
