7ce9ef8a054c5c815c1cbecf088f932bfdae5d7f
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-02-01 01:52:21 +0700

    setup menu, handling favorite click

--- app/src/main/java/com/etwicaksono/infomovie2/ui/homelist/HomeActivity.kt
+++ app/src/main/java/com/etwicaksono/infomovie2/ui/homelist/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.etwicaksono.infomovie2.ui.homelist
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.etwicaksono.infomovie2.R
 import com.etwicaksono.infomovie2.databinding.ActivityHomeListBinding
@@ -23,15 +24,23 @@ class HomeActivity : AppCompatActivity() {
         TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
             tab.text = TAB_TITLES[position]
         }.attach()
-
-        binding.btnFavorite.setOnClickListener {
-            startActivity(Intent(this@HomeActivity, FavoriteActivity::class.java))
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_home, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_favorite -> startActivity(
+                Intent(
+                    this@HomeActivity,
+                    FavoriteActivity::class.java
+                )
+            )
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
