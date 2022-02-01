2a098ce2a5fc991100360f1a67e090d8e21252e4
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-02-01 17:59:19 +0700

    update ListViewModel.kt

--- app/src/test/java/com/etwicaksono/infomovie2/ui/homelist/ListViewModelTest.kt
+++ app/src/test/java/com/etwicaksono/infomovie2/ui/homelist/ListViewModelTest.kt
@@ -3,12 +3,15 @@ package com.etwicaksono.infomovie2.ui.homelist
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.paging.PagedList
+import androidx.paging.PositionalDataSource
 import com.etwicaksono.infomovie2.data.CatalogueRepository
 import com.etwicaksono.infomovie2.data.source.local.entity.ListEntity
 import com.etwicaksono.infomovie2.utils.DataDummy
+import com.etwicaksono.infomovie2.utils.SortUtils
+import com.etwicaksono.infomovie2.valueobject.Resource
 import com.nhaarman.mockitokotlin2.verify
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -16,10 +19,11 @@ import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
+import java.util.concurrent.Executors
 
 @RunWith(MockitoJUnitRunner::class)
 class ListViewModelTest {
-   /* private lateinit var viewModel: ListViewModel
+    private lateinit var viewModel: ListViewModel
 
     private val dummyMovies = DataDummy.getAllMoviesDummy()
     private val dummyTvShows = DataDummy.getAllTvShowsDummy()
@@ -31,7 +35,7 @@ class ListViewModelTest {
     private lateinit var catalogueRepository: CatalogueRepository
 
     @Mock
-    private lateinit var observer: Observer<List<ListEntity>>
+    private lateinit var observer: Observer<Resource<PagedList<ListEntity>>>
 
 
     @Before
@@ -40,36 +44,135 @@ class ListViewModelTest {
     }
 
     @Test
-    fun getAllMovies() {
-        val movies = MutableLiveData<List<ListEntity>>()
-        movies.value = dummyMovies
+    fun `getAllMovies should be success`() {
+        val sort = SortUtils.TITLE
+        val movies = PagedTestDataSources.snapshot(dummyMovies)
+        val expected = MutableLiveData<Resource<PagedList<ListEntity>>>()
+        expected.value = Resource.success(movies)
+        `when`(catalogueRepository.getPopularMovies(sort)).thenReturn(expected)
 
-        `when`(catalogueRepository.getPopularMovies()).thenReturn(movies)
-
-        val dataListMovies = viewModel.getAllMovies().value
-
-        verify(catalogueRepository).getPopularMovies()
-        assertNotNull(dataListMovies)
-        assertEquals(20, dataListMovies?.size)
-
+        viewModel.sort.value = sort
         viewModel.getAllMovies().observeForever(observer)
-        verify(observer).onChanged(dummyMovies)
+        verify(observer).onChanged(expected.value)
+
+        val expectedValue = expected.value
+        val actualValue = viewModel.getAllMovies().value
+
+        assertNotNull(actualValue)
+        assertEquals(expectedValue, actualValue)
+        assertEquals(expectedValue?.data, actualValue?.data)
+        assertEquals(expectedValue?.data?.size, actualValue?.data?.size)
     }
 
     @Test
-    fun getAllTvShows() {
-        val tvShows = MutableLiveData<List<ListEntity>>()
-        tvShows.value = dummyTvShows
+    fun `getAllMovies should be success but data is empty`() {
+        val sort = SortUtils.TITLE
+        val movies = PagedTestDataSources.snapshot()
+        val expected = MutableLiveData<Resource<PagedList<ListEntity>>>()
+        expected.value = Resource.success(movies)
+        `when`(catalogueRepository.getPopularMovies(sort)).thenReturn(expected)
 
-        `when`(catalogueRepository.getPopularTvShow()).thenReturn(tvShows)
+        viewModel.getAllMovies().observeForever(observer)
+        verify(observer).onChanged(expected.value)
 
-        val dataListMovies = viewModel.getAllTvShows().value
+        val actualValueDataSize = viewModel.getAllMovies().value?.data?.size
+        assertTrue(
+            "size of data should be 0, actual is $actualValueDataSize",
+            actualValueDataSize == 0
+        )
+    }
 
-        verify(catalogueRepository).getPopularTvShow()
-        assertNotNull(dataListMovies)
-        assertEquals(20, dataListMovies?.size)
+    @Test
+    fun `getAllMovies should be error`() {
+        val sort = SortUtils.TITLE
+        val expectedMessage = "Error happened"
+        val expected = MutableLiveData<Resource<PagedList<ListEntity>>>()
+        expected.value = Resource.error(expectedMessage, null)
+        `when`(catalogueRepository.getPopularMovies(sort)).thenReturn(expected)
+
+        viewModel.getAllMovies().observeForever(observer)
+        verify(observer).onChanged(expected.value)
+
+        val actualMessage = viewModel.getAllMovies().value?.message
+        assertEquals(expectedMessage,actualMessage)
+    }
+    @Test
+    fun `getAllTvShows should be success`() {
+        val sort = SortUtils.TITLE
+        val tvShow = PagedTestDataSources.snapshot(dummyTvShows)
+        val expected = MutableLiveData<Resource<PagedList<ListEntity>>>()
+        expected.value = Resource.success(tvShow)
+        `when`(catalogueRepository.getPopularTvShow(sort)).thenReturn(expected)
+
+        viewModel.sort.value = sort
+        viewModel.getAllTvShows().observeForever(observer)
+        verify(observer).onChanged(expected.value)
+
+        val expectedValue = expected.value
+        val actualValue = viewModel.getAllTvShows().value
+
+        assertNotNull(actualValue)
+        assertEquals(expectedValue, actualValue)
+        assertEquals(expectedValue?.data, actualValue?.data)
+        assertEquals(expectedValue?.data?.size, actualValue?.data?.size)
+    }
+
+    @Test
+    fun `getAllTvShows should be success but data is empty`() {
+        val sort = SortUtils.TITLE
+        val tvShow = PagedTestDataSources.snapshot()
+        val expected = MutableLiveData<Resource<PagedList<ListEntity>>>()
+        expected.value = Resource.success(tvShow)
+        `when`(catalogueRepository.getPopularTvShow(sort)).thenReturn(expected)
 
         viewModel.getAllTvShows().observeForever(observer)
-        verify(observer).onChanged(dummyTvShows)
-    }*/
+        verify(observer).onChanged(expected.value)
+
+        val actualValueDataSize = viewModel.getAllTvShows().value?.data?.size
+        assertTrue(
+            "size of data should be 0, actual is $actualValueDataSize",
+            actualValueDataSize == 0
+        )
+    }
+
+    @Test
+    fun `getAllTvShows should be error`() {
+        val sort = SortUtils.TITLE
+        val expectedMessage = "Error happened"
+        val expected = MutableLiveData<Resource<PagedList<ListEntity>>>()
+        expected.value = Resource.error(expectedMessage, null)
+        `when`(catalogueRepository.getPopularTvShow(sort)).thenReturn(expected)
+
+        viewModel.getAllTvShows().observeForever(observer)
+        verify(observer).onChanged(expected.value)
+
+        val actualMessage = viewModel.getAllTvShows().value?.message
+        assertEquals(expectedMessage,actualMessage)
+    }
+
+
+    class PagedTestDataSources private constructor(private val items: List<ListEntity>) :
+        PositionalDataSource<ListEntity>() {
+        override fun loadInitial(
+            params: LoadInitialParams,
+            callback: LoadInitialCallback<ListEntity>
+        ) {
+            callback.onResult(items, 0, items.size)
+        }
+
+        override fun loadRange(params: LoadRangeParams, callback: LoadRangeCallback<ListEntity>) {
+            val start = params.startPosition
+            val end = params.startPosition + params.loadSize
+            callback.onResult(items.subList(start, end))
+        }
+
+        companion object {
+            fun snapshot(items: List<ListEntity> = listOf()): PagedList<ListEntity> {
+                return PagedList.Builder(PagedTestDataSources(items), 10)
+                    .setNotifyExecutor(Executors.newSingleThreadExecutor())
+                    .setFetchExecutor(Executors.newSingleThreadExecutor())
+                    .build()
+            }
+        }
+    }
 }
